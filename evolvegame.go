0cc9e54a89a201a4a358eb82318623ccc57d0e24
Trumae <trumae@gmail.com>
2014-02-03 05:52:38 -0500

    draw world

--- evolvegame.go
+++ evolvegame.go
@@ -1,7 +1,15 @@
 package main
 
 import "fmt"
+import "math/rand"
+import "time"
+import eg "github.com/trumae/evolvegamelib"
 
 func main() {
-	fmt.Print("Hello\n")
+	fmt.Print("Hello 2\n")
+        rand.Seed(time.Now().UnixNano())
+	a := eg.NewArenaSample()
+	a.Evolution(200)
+        a.DrawWorld()
 }
+
