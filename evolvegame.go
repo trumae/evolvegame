7ca1185c263707f4fd34e813e942a2846605e6bc
Trumae <trumae@gmail.com>
2014-02-04 16:51:47 -0500

    seed as parameter

--- evolvegame.go
+++ evolvegame.go
@@ -1,22 +1,23 @@
 package main
 
 import (
- "fmt"
- "flag"
- "math/rand"
- "time"
- eg "github.com/trumae/evolvegamelib"
+	"flag"
+	"fmt"
+	eg "github.com/trumae/evolvegamelib"
+	"math/rand"
+	"time"
 )
 
 var numCycles int
+var seed int64
 
 func main() {
-        fmt.Print("EvolveGame\n\n")
+	fmt.Print("EvolveGame\n\n")
 	flag.IntVar(&numCycles, "numCycles", 100, "Number of evolution cycles (default: 100)")
+	flag.Int64Var(&seed, "seed", time.Now().UnixNano(), "seed for random number (default: now)")
 	flag.Parse()
-        rand.Seed(time.Now().UnixNano())
+	rand.Seed(seed)
 	a := eg.NewArenaSample()
 	a.Evolution(numCycles)
-        a.DrawWorld()
+	a.DrawWorld()
 }
-
